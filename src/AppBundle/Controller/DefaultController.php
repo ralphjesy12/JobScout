8d5014f9739f632d00967d7e68b6ce40f37ab3df
Ralph John Galindo <ralphjesy@gmail.com>
2017-03-13 17:18:52 +0800

    Saving and Displaying Tasks

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\Controller;
+use AppBundle\Entity\Task;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -14,8 +15,49 @@ class DefaultController extends Controller
     */
     public function todosAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('todos/index.html.twig');
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        $repository = $this->getDoctrine()
+        ->getRepository('AppBundle:Task');
+
+        $query = $repository->createQueryBuilder('t')
+        ->where('t.userId = :user')
+        ->setParameter('user',$user->getId())
+        ->orderBy('t.createdAt', 'DESC')
+        ->getQuery();
+
+        $tasks = $query->getResult();
+
+        return $this->render('todos/index.html.twig',compact(['user','tasks']));
+    }
+
+    /**
+    * @Route("/todo/create", name="todo_create")
+    */
+    public function todoCreateAction(Request $request)
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        return $this->render('todos/create.html.twig',compact(['user']));
+    }
+
+    /**
+    * @Route("/todo/save", name="todo_save")
+    */
+    public function todoSaveAction(Request $request)
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $task = new Task();
+        $task->setContent($request->get('content'));
+        $task->setCreatedAt(date('Y-m-d H:i:s'));
+        $task->setUserId($user->getId());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($task);
+        $em->flush();
+        return $this->redirectToRoute('todos');
     }
 
     public function adminAction()
---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Task
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Task
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = new \DateTime();
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Task
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = new \DateTime($updatedAt);
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Task
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+}
